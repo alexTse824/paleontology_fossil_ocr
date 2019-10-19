c216a482d06a444283501d17aeb471c698c7f883
xie <harryxie123@hotmail.com>
2019-10-19 17:57:09 +0800

    restore: file name format function;

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 DIR_base = os.path.split(__file__)[0]
 DIR_data = os.path.join(DIR_base, 'data')
 
-# db settings
+# MongoDB settings
 DB_address_ip = 'localhost'
 DB_address_port = 27017
 DB_name = 'paleontology_fossils_dataset'
--- test/test_preprocess/test_file_handler.py
+++ test/test_preprocess/test_file_handler.py
@@ -1,7 +1,6 @@
-import os
 import unittest
 
-from utils.file_handler import file_name_format, generate_ds_label_path
+from preprocess.file_handler import file_name_format
 
 
 class TestUtils(unittest.TestCase):
@@ -11,11 +10,6 @@ class TestUtils(unittest.TestCase):
         for i in range(9):
             current_path = os.path.join(target_dir, f'class_{i}')
             file_name_format(current_path)
-
-    def test_generate_ds_label_path(self):
-        path = '/Users/xie/Code/NJU/paleontology_fossil_ocr/data/raw_data/class_0'
-        for i in generate_ds_label_path(path):
-            print(i)
 
 
 if __name__ == "__main__":
