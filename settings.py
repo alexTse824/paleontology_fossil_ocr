ac2457855b4a670935d2630c12b47c23da08ad70
xie <harryxie123@hotmail.com>
2019-10-19 11:23:00 +0800

    initial commit

---
+++ settings.py
@@ -0,0 +1,4 @@
+import os
+
+base_dir = os.path.split(__file__)[0]
+data_dir = os.path.join(base_dir, 'data')
---
+++ utils.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+
+def file_name_format(path):
+    """change filename in target directory to serialized number"""
+    os.chdir(path)
+    file_list = os.listdir(path)
+
+    index = 0
+    for file_name in file_list:
+        pattern = re.findall('.+.(jpg|png|gif|JPG|jpeg)', file_name)
+        if pattern:
+            os.rename(file_name, f'{index}.jpg')
+            index += 1
+        else:
+            print(file_name)
