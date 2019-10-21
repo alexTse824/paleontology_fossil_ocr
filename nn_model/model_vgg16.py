608eca9efb230ba55ec102f59d97debd5c0cc7a1
xie <harryxie123@hotmail.com>
2019-10-21 11:28:48 +0800

    add: vgg16 nn model;

--- nn_model/model_vgg16.py
+++ nn_model/model_vgg16.py
@@ -1,13 +1,11 @@
-'''
-Model By: https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
-'''
 import os
-from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import VGG16
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.optimizers import SGD
 
-from nn_model.sequential_model import Sequential
 from settings import DIR_straitified_dataset
-
 
 # fix OMP: Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.
 os.environ["KMP_DUPLICATE_LIB_OK"] = 'TRUE'
@@ -21,7 +19,8 @@ VALIDATION_SAMPLE_NUM = 0
 for root, dir_name, file_name in os.walk(os.path.join(DATASET_DIR, 'train')):
     TRAIN_SAMPLE_NUM += len(file_name)
 
-for root, dir_name, file_name in os.walk(os.path.join(DATASET_DIR, 'validation')):
+for root, dir_name, file_name in os.walk(
+        os.path.join(DATASET_DIR, 'validation')):
     VALIDATION_SAMPLE_NUM += len(file_name)
 
 BATCH_SIZE = 64
@@ -30,49 +29,49 @@ EPOCHS = 50
 IMG_WIDTH = 224
 IMG_HEIGHT = 224
 
-model = Sequential(STRAITIFIED_DIR_NAME)
+model = Sequential()
+model.add(
+    VGG16(include_top=False,
+          weights='imagenet',
+          input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)))
 
-model.add(Conv2D(32, (3, 3), input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+top_model = Sequential()
+top_model.add(Flatten(input_shape=model.output_shape[1:]))
+top_model.add(Dense(256, activation='relu'))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(9, activation='softmax'))
+model.add(top_model)
 
-model.add_conv_act_maxpool_2x2(32, (3, 3), 'relu', (2, 2))
-model.add_conv_act_maxpool_2x2(64, (3, 3), 'relu', (2, 2))
+sgd = SGD(lr=1e-4)
 
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
+model.compile(optimizer=sgd,
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(rescale=1. / 255,
                                    shear_range=0.2,
                                    zoom_range=0.2,
                                    horizontal_flip=True)
-validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
+validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     os.path.join(DATASET_DIR, 'train'),
     target_size=(IMG_WIDTH, IMG_HEIGHT),
     batch_size=BATCH_SIZE,
-    class_mode='binary',
-    save_to_dir=os.path.join(DATASET_DIR, 'train', 'preprocess', 'train'))
+    class_mode='categorical',
+    shuffle=True)
+
 validation_generator = validation_datagen.flow_from_directory(
     os.path.join(DATASET_DIR, 'validation'),
     target_size=(IMG_WIDTH, IMG_HEIGHT),
     batch_size=BATCH_SIZE,
-    class_mode='binary')
+    class_mode='categorical',
+    shuffle=True)
 
 model.fit_generator(train_generator,
                     steps_per_epoch=TRAIN_SAMPLE_NUM // BATCH_SIZE,
                     epochs=EPOCHS,
                     validation_data=validation_generator,
-                    validation_steps=VALIDATION_SAMPLE_NUM // BATCH_SIZE)
-
-model.save_weights(f'{STRAITIFIED_DIR_NAME}.weights.h5')
+                    validation_steps=VALIDATION_SAMPLE_NUM // BATCH_SIZE,
+                    verbose=2)
